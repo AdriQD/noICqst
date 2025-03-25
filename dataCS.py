00d95a6e60f9293aba3ad66e429ca30865865438
AdriQD <46886493+AdriQD@users.noreply.github.com>
2025-03-25 11:58:45 +0100

    updates data generation for Leonardo

--- dataCS.py
+++ dataCS.py
@@ -11,9 +11,9 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 
-NumQubits = config['DATA']['NumQubit']
-NumShots = config['DATA']['NumShots']
-NumPauliStrings = config['DATA']['NumPauliStrings']
+NumQubits = int(config['DATA']['NumQubits'])
+NumShots = int(config['DATA']['NumShots'])
+NumPauliStrings = int(config['DATA']['NumPauliStrings'])
 path = config['DATA']['PauliFilePath']
 
 
@@ -24,8 +24,9 @@ def generate_unique_base3_numbers(N, length,path):
 	Generate a list of N unique random base-3 numbers (strings of digits 0, 1, 2).
 	
 	Parameters:
-		N (int): Number of unique numbers to generate.
-		length (int): Length of each number.
+		N (int): Total number of POVM/correlators to get to generate.
+		length (int): Length of each number, aka num of qubits.
+		path : file's saving path.
 	
 	Returns:
 		List[str]: A list of N unique base-3 numbers as strings.
@@ -42,7 +43,8 @@ def generate_unique_base3_numbers(N, length,path):
 		num = ''.join(random.choice('012') for _ in range(length))
 		generated.add(num)
 	
-	np.save(path, generated, allow_pickle=True)
+	generated = list(generated)
+	np.save(path, np.array(generated), allow_pickle=True)
 
 
 class CSdata():
@@ -53,9 +55,9 @@ class CSdata():
 		
 		self.NumSh = NumShots
 		self.nps = NumPauliStrings
-		self.path = path
+		self.path = path+'.npy'
 
-	def string2paulibasis(self):
+	def string2paulibasis(self, path):
 		"""
 		Given a list of binary strings, gives the Pauli bases. 
 		PARAMETERS:
@@ -65,7 +67,7 @@ class CSdata():
 				  the bases associated to the Pauli strings in idx.
 		"""
 
-		idx = np.load(self.path, allow_pickle=True)
+		idx = np.load(path, allow_pickle=True)
 		# define the one-qubit bases
 		X = np.array([[1., 1.], [1., -1.]], dtype=complex)/np.sqrt(2)
 		Y = np.array([[1, 1], [1.*1j, -1.*1j]], dtype=complex)/np.sqrt(2) 
@@ -152,7 +154,7 @@ class CSdata():
 					   - prob.reshape(-1, 1)) <= epsilon]
 		objective = cp.Minimize(cp.normNuc(de))
 		problema = cp.Problem(objective, constraints)
-		result = problema.solve()
+		result = problema.solve(solver=cp.SCS)
 
 		return de.value/np.trace(de.value)
 
@@ -212,3 +214,22 @@ class CSdata():
 			result = pool.map(csReconstruction, DMiterators)
 
 		return result
+
+if __name__ == "__main__":
+
+	#testing the generation of random pauli strings
+	generate_unique_base3_numbers(NumPauliStrings,NumQubits,path)
+	o = np.load(path+'.npy', allow_pickle=True)
+	print(o)
+
+	#testing density matrix generation
+	ob = CSdata(NumQubits,NumShots,NumPauliStrings,path)
+	d = ob.density_matrix()
+	print(d.shape),print(np.trace(d).real)
+
+	netdata = ob.GenerateData(d,False)
+	print
+	print("fidelityWITHshots: ", np.round(np.abs(np.trace(netdata['csdm']@netdata['thdm'])), 3))
+
+
+
